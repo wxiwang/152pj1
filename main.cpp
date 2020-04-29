18a204f89c110788e47e0d06f156dc852dcadbd7
wxiwang <wxiwang@ucdavis.edu>
2020-04-29 10:14:14 -0700

    edit main function

--- main.cpp
+++ main.cpp
@@ -33,60 +33,16 @@ int main() {
 
     transmitter host(MAXBUFFER);//The transmitter object
     std::list<event> items;//The Global Event List
-    //event init(time+nedt(lambda),true,1);
-    //items.push_back(init);
-    //time = init.time;
+
     int arrival_num = 0;//The number of arriving packet
     int departure_num = 0;//The number of departing packet
 
-    /*event new_departure(0,false,0);//Declare before we use it. Actually it's virtual.
-    for(int i = 0; i < 10;i++){
-        if(i==3){
-            int j=0;
-        }
-        time=time+nedt(lambda);//Time of the arrival event
-        event new_arrival(time,true,i,host.queueLength(),host.getPacketLoss());
 
-
-        if(!host.isBusy()){
-            departure_time = time + nedt(mu);
-            host.current_packet = i;
-            //items.push_back(new_arrival);
-            new_departure.set(departure_time,false,host.getCurrentPacket(),host.queueLength(),host.getPacketLoss());
-        }//if the transmitter is not busy, just transmit the packet
-        else if(!host.bufferIsFull()){
-            if(host.current_packet!=new_arrival.number)host.buffer.push(new_arrival.number);
-            //items.push_back(new_arrival);
-        }// else if it's not full, enqueue the  packet
-        else host.packet_loss++;//else packet loss happens
-
-        if(time>departure_time && departure_time>0){
-            items.push_back(new_departure);
-            if(!host.buffer.empty()){
-                host.current_packet = host.buffer.front();
-                host.buffer.pop();
-            }
-            else host.current_packet=0;
-            departure_time += nedt(mu);
-            new_departure.set(departure_time,false,host.getCurrentPacket(),host.queueLength(),host.getPacketLoss());
-
-            i--;
-            continue;
-        }//Only push departure event when all the arrival events before it are pushed so that the order of GEL is well maintained.
-        if(!host.bufferIsFull())items.push_back(new_arrival);
-    }*/
-    //event cur_arrival(0,1,0);
-    //event cur_departure(0,2,0);
     event cur_event(0,false,0);//(cur_arrival>cur_departure)?cur_departure:cur_arrival;
     double departure_time=0;
     double arrival_time=0;
     arrival_time += nedt(lambda);
     for(int i = 0; i < 150000; i++){
-
-        //if(host.buffer.empty() && cur_event.type)departure_time=arrival_time+nedt(mu);
-        //else if(host.isBusy())departure_time+=nedt(mu);
-        //arrival_time += nedt(lambda);
-        //if(!host.isBusy())departure_time=arrival_time+nedt(mu);
 
         if(!cur_event.type){
             if(!host.isBusy())departure_time = arrival_time + nedt(mu);
@@ -96,7 +52,7 @@ int main() {
             arrival_time += nedt(lambda);
         }//else we only update the arrival time
 
-
+        //If arrival time is less than departure time, it means the arrival event takes place before the departure event so we push the arrival event to GEL.
         if(arrival_time<departure_time && arrival_num<50000){
             arrival_num++;
             if(!host.bufferIsFull())cur_event.set(arrival_time,true,arrival_num,cur_event.queueLength+1,cur_event.totalPacketLoss);
@@ -112,21 +68,21 @@ int main() {
 
             cur_event.set(departure_time, false, host.current_packet, cur_event.queueLength - 1,cur_event.totalPacketLoss);
             items.push_back(cur_event);
-            if(host.current_packet==50000)break;
-            host.current_packet = 0;
+            if(host.current_packet==50000)break;//In the experiment we simulate transmitting 50000 packets.
+            host.current_packet = 0;//server completes transmitting
             if(!host.buffer.empty()){
                 host.current_packet = host.buffer.front();
                 host.buffer.pop();//enqueue the buffer
             }
         }
-        //If arrival time is less than departure time, it means the arrival event takes place before the departure event so we push the arrival event to GEL.
         if(departure_num == 50000)break;
     }
 
     for(auto & item : items){
-        item.print();
+        item.print();//print out event information to the screen
     }
 
+    //write to file
     std::string filePath ="projectData/lambda="+std::to_string(lambda).substr(0,3)+".txt";
     if(MAXBUFFER<100){
         filePath = "projectData/lambda="+std::to_string(lambda).substr(0,3)+",MAXBUFFER="+std::to_string(MAXBUFFER)+".txt";
@@ -138,26 +94,5 @@ int main() {
     for(auto & item : items){
         out<<item.time<<","<<item.type<<","<<item.number<<","<<item.queueLength<<","<<item.totalPacketLoss<<std::endl;
     }
-    /*std::priority_queue<event,std::vector<event>,std::greater<> > items;
-    event init(time+nedt(lambda),true,1);
-    items.push(init);
-
-    int num = 1;
-
-    for(int i = 0; i < 10; i++){
-        event currentEvent = items.top();
-        items.pop();
-        currentEvent.print();
-        time = currentEvent.time;
-        if(currentEvent.type){
-            event nextEvent(time+nedt(mu),false,currentEvent.number);
-            items.push(nextEvent);
-        }
-        else{
-            num+=1;
-            event arrivalEvent(time+nedt(lambda),true,num);
-            items.push(arrivalEvent);
-        }
-    }*/
     return 0;
 }
